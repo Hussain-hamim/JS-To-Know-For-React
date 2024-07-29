3c5f2d472b4d39f16edbc93f3b9a4410c3926048
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-29 15:13:03 +0430

    Nullish coalescing operator

--- src/App.js
+++ src/App.js
@@ -1,48 +1,26 @@
-export default function add(a, b) {
+// here's what we often did for this:
+x = x || "some default";
+
+// but this was problematic for numbers or booleans where "0" or "false" are valid values
+
+// So, if we wanted to support this:
+add(null, 3);
+
+// here's what we had to do before:
+function add(a, b) {
+  a = a == null ? 0 : a;
+  b = b == null ? 0 : b;
   return a + b;
 }
 
-/*
- * import add from './add'
- * console.assert(add(3, 2) === 5)
- */
-
-export const foo = "bar";
-
-/*
- * import {foo} from './foo'
- * console.assert(foo === 'bar')
- */
-
-export function subtract(a, b) {
-  return a - b;
+// here's what we can do now
+function add(a, b) {
+  a = a ?? 0;
+  b = b ?? 0;
+  return a + b;
 }
 
-export const now = new Date();
-
-/*
- * import {subtract, now} from './stuff'
- * console.assert(subtract(4, 2) === 2)
- * console.assert(now instanceof Date)
- */
-
-// dynamic imports
-import("./some-module").then(
-  (allModuleExports) => {
-    // the allModuleExports object will be the same object you get if you had
-    // used: import * as allModuleExports from './some-module'
-    // the only difference is this will be loaded asynchronously which can
-    // have performance benefits in some cases
-  },
-  (error) => {
-    // handle the error
-    // this will happen if there's an error loading or running the module
-  }
-);
-
 // in React:
-import React, { Suspense, Fragment } from "react";
-
-// dynamic import of a React component
-const BigComponent = React.lazy(() => import("./big-component"));
-// big-component.js would need to "export default BigComponent" for this to work
+function DisplayContactName({ contact }) {
+  return <div>{contact.name ?? "Unknown"}</div>;
+}
