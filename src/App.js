c9259561ac4c94e61b0a600df8f26ac4f0a473db
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-29 12:48:56 +0430

    array methods

--- src/App.js
+++ src/App.js
@@ -1,30 +1,73 @@
-const message = bottle.fullOfSoda
-  ? "The bottle has soda!"
-  : "The bottle may not have soda :-(";
+const dogs = [
+  {
+    id: "dog-1",
+    name: "Poodle",
+    temperament: [
+      "Intelligent",
+      "Active",
+      "Alert",
+      "Faithful",
+      "Trainable",
+      "Instinctual",
+    ],
+  },
 
-// is the same as
-let message;
-if (bottle.fullOfSoda) {
-  message = "The bottle has soda!";
-} else {
-  message = "The bottle may not have soda :-(";
-}
+  {
+    id: "dog-2",
+    name: "Bernese Mountain Dog",
+    temperament: ["Affectionate", "Intelligent", "Loyal", "Faithful"],
+  },
+
+  {
+    id: "dog-3",
+    name: "Labrador Retriever",
+    temperament: [
+      "Intelligent",
+      "Even Tempered",
+      "Kind",
+      "Agile",
+      "Outgoing",
+      "Trusting",
+      "Gentle",
+    ],
+  },
+];
+
+dogs.find((dog) => dog.name === "Bernese Mountain Dog");
+// {id: 'dog-2', name: 'Bernese Mountain Dog', ...etc}
+
+dogs.some((dog) => dog.temperament.includes("Aggressive"));
+// false
+
+dogs.some((dog) => dog.temperament.includes("Trusting"));
+// true
+
+dogs.every((dog) => dog.temperament.includes("Trusting"));
+// false
+
+dogs.every((dog) => dog.temperament.includes("Intelligent"));
+// true
+
+dogs.map((dog) => dog.name);
+// ['Poodle', 'Bernese Mountain Dog', 'Labrador Retriever']
+
+dogs.filter((dog) => dog.temperament.includes("Faithful"));
+// [{id: 'dog-1', ..etc}, {id: 'dog-2', ...etc}]
+
+dogs.reduce((allTemperaments, dog) => {
+  return [...allTemperaments, ...dog.temperament];
+}, []);
+// [ 'Intelligent', 'Active', 'Alert', ...etc ]
 
 // in React:
-function TeddyBearList({ teddyBears }) {
+function RepositoryList({ repositories, owner }) {
   return (
-    <React.Fragment>
-      {teddyBears.length ? (
-        <ul>
-          {teddyBears.map((teddyBear) => (
-            <li key={teddyBear.id}>
-              <span>{teddyBear.name}</span>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <div>There are no teddy bears. The sadness.</div>
-      )}
-    </React.Fragment>
+    <ul>
+      {repositories
+        .filter((repo) => repo.owner === owner)
+        .map((repo) => (
+          <li key={repo.id}>{repo.name}</li>
+        ))}
+    </ul>
   );
 }
