11fc4b71cdc68a03804c85eca62031b564f82700
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-29 12:11:25 +0430

    1. template literal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
-};
+  // Template literal:
+  const greeting = "Hello";
+  const subject = "World";
+  console.log(`${greeting} ${subject}!`); // Hello World!
 
+  // this is the same as:
+  console.log(greeting + " " + subject + "!");
+
+  return <div>hello</div>;
+};
 export default App;
+
+// in React:
+function Box({ className, ...props }) {
+  return <div className={`box ${className}`} {...props} />;
+}
+// Template literals are like regular strings with super-powers:
