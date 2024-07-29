c5431cc6bcb22718ef1ec50b4b977878f1a4fbbb
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-29 11:54:27 +0430

    Template literal

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+const greeting = "Hello";
+const subject = "World";
+console.log(`${greeting} ${subject}!`); // Hello World!
+
+// this is the same as:
+console.log(greeting + " " + subject + "!");
+
+// in React:
+function Box({ className, ...props }) {
+  return <div className={`box ${className}`} {...props} />;
+}
