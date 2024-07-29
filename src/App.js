396591a79782fed33e2ad1c9103c474a757c2f29
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-29 12:15:36 +0430

    2. Arrow Function

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import React from "react";
+const getFive = () => 5;
+const addFive = (a) => a + 5;
+const divide = (a, b) => a / b;
 
-const App = () => {
-  // Template literal:
-  const greeting = "Hello";
-  const subject = "World";
-  console.log(`${greeting} ${subject}!`); // Hello World!
-
-  // this is the same as:
-  console.log(greeting + " " + subject + "!");
-
-  return <div>hello</div>;
-};
-export default App;
+// this is the same as:
+function getFive() {
+  return 5;
+}
+function addFive(a) {
+  return a + 5;
+}
+function divide(a, b) {
+  return a / b;
+}
 
 // in React:
-function Box({ className, ...props }) {
-  return <div className={`box ${className}`} {...props} />;
+export default function TeddyBearList({ teddyBears }) {
+  return (
+    <ul>
+      {teddyBears.map((teddyBear) => (
+        <li key={teddyBear.id}>
+          <span>{teddyBear.name}</span>
+        </li>
+      ))}
+    </ul>
+  );
 }
-// Template literals are like regular strings with super-powers:
