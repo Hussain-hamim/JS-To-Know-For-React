a6ece55d158999acb74abee10d624f71ba10b354
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-08-01 07:19:10 +0430

    cleanup fn

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 
-export default function FavoriteColor() {
-  const [color, setColor] = useState("red");
+function Timer() {
+  const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <h1>My favorite color is {color}!</h1>
-      <button type="button" onClick={() => setColor("blue")}>
-        Blue
-      </button>
-      <button type="button" onClick={() => setColor("red")}>
-        Red
-      </button>
-      <button type="button" onClick={() => setColor("pink")}>
-        Pink
-      </button>
-      <button type="button" onClick={() => setColor("green")}>
-        Green
-      </button>
-    </>
-  );
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return <h1>I've rendered {count} times!</h1>;
 }
