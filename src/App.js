2d959605510b05d0db5466045b8e3de3f607cb49
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-08-01 07:18:11 +0430

    fav color fn

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
 
-const App = () => {
-  const [count, setCount] = useState(0);
+export default function FavoriteColor() {
+  const [color, setColor] = useState("red");
 
   return (
     <>
-      Count: {count}
-      <div>welcome to our site</div>
-      <button onClick={() => setCount(count + 1)}>Count +</button>
+      <h1>My favorite color is {color}!</h1>
+      <button type="button" onClick={() => setColor("blue")}>
+        Blue
+      </button>
+      <button type="button" onClick={() => setColor("red")}>
+        Red
+      </button>
+      <button type="button" onClick={() => setColor("pink")}>
+        Pink
+      </button>
+      <button type="button" onClick={() => setColor("green")}>
+        Green
+      </button>
     </>
   );
-};
-
-export default App;
+}
